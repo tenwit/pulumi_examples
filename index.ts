35b1a881e3143e801d93db5358539b19122705eb
Paul <tenwit@gmail.com>
2020-05-06 09:29:27 +1200

    Demonstrate registerStackTransformation
    First registers a simple stack transformation based on code found on
    Joe's Pulumi blog:
    https://www.pulumi.com/blog/automatically-enforcing-aws-resource-tagging-policies/#automatically-applying-tags
    
    Then creates a simple VPC using Pulumi CrossWalk, which creates a
    selection of taggable resources that we expect to see tags on.
    
    Not currently working. The resources are correctly created by CrossWalk
    but there are no tags. No idea why, yet.

---
+++ index.ts
@@ -0,0 +1,26 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+import { registerAutoTags } from "./autotag";
+
+// Automatically inject tags.
+registerAutoTags({
+  "user:Project": pulumi.getProject(),
+  "user:Stack": pulumi.getStack(),
+  "Env": "PoC",
+});
+
+const vpc = new awsx.ec2.Vpc("proof_of_concept", {
+  cidrBlock: "192.168.96.0/23",
+  numberOfAvailabilityZones: 2,
+  numberOfNatGateways: 0,
+  subnets: [
+    { type: "public", name: "bastion" },
+    { type: "isolated", name: "private" }
+  ]
+});
+
+export const vpc_poc_id = vpc.id;
+export const vpc_poc_PrivateSubnetIds = vpc.privateSubnetIds;
+export const vpc_poc_PublicSubnetIds = vpc.publicSubnetIds;
